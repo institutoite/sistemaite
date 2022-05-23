a27ee581698c73544d34878eceb7690c296f2783
institutoite <informaciones.ite@gmail.com>
2022-05-23 08:05:30 -0400

    Empezando motivo

--- resources/views/modalidad/show.blade.php
+++ resources/views/modalidad/show.blade.php
@@ -1,45 +1,68 @@
 @extends('adminlte::page')
 @section('css')
-    <link rel="stylesheet" href="{{asset('bootstrap/css/bootstrap.css')}}">
+    <link rel="stylesheet" href="{{asset('dist/css/bootstrap/bootstrap.css')}}">
 @stop
 
-@section('title', 'Modalidad mostrar')
+@section('title', 'Mostrar Feriado')
+
+
+@section('content_header')
+    <h1 class="text-center text-primary">Mostrar Modalidad</h1>
+@stop
+
 @section('content')
-    <section class="content container-fluid">
         <div class="row">
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-header bg-secondary">
                         <div class="float-left">
-                            <span class="card-title">Show Modalidad</span>
+                            <span class="card-title">Mostrar Modalidad</span>
                         </div>
                         <div class="float-right">
-                            <a class="btn btn-primary" href="{{ route('modalidads.index') }}"> Atrás</a>
+                            <a class="btn btn-primary" href="{{ route('modalidads.index') }}"> Listar Modalidades </a>
                         </div>
                     </div>
 
                     <div class="card-body">
-                        
-                        <div class="form-group">
-                            <strong>Modalidad:</strong>
-                            {{ $modalidad->modalidad }}
-                        </div>
-                        <div class="form-group">
-                            <strong>Costo:</strong>
-                            {{ $modalidad->costo }}
-                        </div>
-                        <div class="form-group">
-                            <strong>Cargahoraria:</strong>
-                            {{ $modalidad->cargahoraria }}
-                        </div>
-                        <div class="form-group">
-                            <strong>Nivel:</strong>
-                            {{ $nivel->nivel }}
-                        </div>
-
+                        <table class="table table-striped table-bordered table-hover">
+                            <thead class="bg-primary">
+                                <tr>
+                                    <th>ATRIBUTO</th>
+                                    <th>VALOR</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td> <strong>ID</strong></td>
+                                    <td>{{ $modalidad->id }}</td>
+                                </tr>
+                                <tr>
+                                    <td> <strong>Modalidad</strong></td>
+                                    <td>{{ $modalidad->modalidad }}</td>
+                                </tr>
+                                <tr>
+                                    <td> <strong>Costo</strong></td>
+                                    <td>{{ $modalidad->costo }}</td>
+                                </tr>
+                                <tr>
+                                    <td> <strong>CargaHoraria</strong></td>
+                                    <td>{{ $modalidad->cargahoraria }}</td>
+                                </tr>
+                                <tr>
+                                    <td> <strong>Nivel</strong></td>
+                                    <td>{{ $nivel->nivel }}</td>
+                                </tr>
+                                <tr>
+                                    <td>Usuario</td>
+                                    <td>
+                                        {{$user->name}}
+                                        <img  src="{{URL::to('/').Storage::url("$user->foto")}}" alt="{{$user->name}}" class="rounded img-thumbnail img-fluid border-primary border-5" width="100"> 
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table> 
                     </div>
                 </div>
             </div>
         </div>
-    </section>
 @endsection
