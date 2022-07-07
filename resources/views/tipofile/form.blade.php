95b587adf89986fc4406dd459d2db0211b7ccb6e
institutoite <informaciones.ite@gmail.com>
2022-07-07 07:58:12 -0400

    tipo file validado

--- resources/views/tipofile/form.blade.php
+++ resources/views/tipofile/form.blade.php
@@ -1,4 +1,11 @@
 <div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 input-group text-sm">    
+        @if($errors->has('tipofile'))
+        <span class="text-danger"> {{ $errors->first('tipofile')}}</span>
+        @endif
+    </div>
+</div>
+<div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" >
         <div class="form-floating mb-3 text-gray">
             <input  type="text" name="tipofile" id="tipofile"  class="form-control @error('tipofile') is-invalid @enderror" value="" autocomplete="off">
@@ -6,6 +13,15 @@
         </div>
     </div>
 </div> 
+
+<div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 input-group text-sm">    
+        @if($errors->has('programa'))
+        <span class="text-danger"> {{ $errors->first('programa')}}</span>
+        @endif
+    </div>
+</div>
+
 <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" >
         <div class="form-floating mb-3 text-gray">
