cf1e629faf0d0b73da960f654d46a8dfd16b35e0
edward <eduardcp20@gmail.com>
2022-10-01 12:24:16 -0400

    01/10/2022
    actualizacion

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -12,19 +12,11 @@
                 <img src="{{URL::to('/')}}/storage/{{$docente->persona->foto}}" alt="Image" class="cs-zoom_item">
             </a>
             <div class="cs-card_info">
-                <a href="#" class="cs-avatar cs-white_bg">
-                <img src="fronted/assets/img/avatar/avatar_12.png" alt="Avatar">
-                <span>{{$docente->perfil}} </span>
-                </a>
                 <h3 class="cs-card_title"><a href="#"></a>{{$docente->persona->nombre}} {{$docente->persona->apellidop}}</h3>
-                <div class="cs-card_price">Fecha Ingreso: <b class="cs-primary_color">03/05/2003</b></div>
+                <div class="cs-card_price">{{$docente->perfil}}</div>
                 <hr>
                 <div class="cs-card_footer">
-                {{-- <span class="cs-card_btn_1" data-modal="#history_1">
-                    <i class="fas fa-redo fa-fw"></i>
-                    View History
-                </span> --}}
-                <span class="cs-card_btn_2" data-modal="#bid_1"><span>Leer mas...</span></span>
+                
                 </div>
             </div>
             </div>
