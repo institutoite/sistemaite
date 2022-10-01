fd6b56794434daf5d191130cedca9b522a1b2ace
edward <eduardcp20@gmail.com>
2022-10-01 17:53:05 -0400

    01/10/2022
    actualizacion

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,28 +1,25 @@
 @extends('layouts.home')
 
 @section('docente')
+
     @foreach ($docentes as $docente)
         <div class="cs-slide">
             <div class="cs-card cs-style4 cs-box_shadow cs-white_bg">
-            {{-- <span class="cs-card_like cs-primary_color">
-                <i class="fas fa-heart fa-fw"></i> 
-                2.1K
-            </span> --}}
-            <a href="#" class="cs-card_thumb cs-zoom_effect">
-                <img src="{{URL::to('/')}}/storage/{{$docente->persona->foto}}" alt="Image" class="cs-zoom_item">
-            </a>
-            <div class="cs-card_info">
-                <h3 class="cs-card_title"><a href="#"></a>{{$docente->persona->nombre}} {{$docente->persona->apellidop}}</h3>
-                <div class="cs-card_price">{{$docente->perfil}}</div>
-                <hr>
-                <div class="cs-card_footer">
-                
+                <a href="#" class="cs-card_thumb cs-zoom_effect">
+                    <img src="{{URL::to('/')}}/storage/{{$docente->persona->foto}}" alt="Image" class="cs-zoom_item">
+                </a>
+                <div class="cs-card_info">
+                    <a href="#" class="cs-avatar cs-white_bg">
+                    <span></span>
+                    </a>
+                    <h3 class="cs-card_title">{{$docente->persona->nombre}} {{$docente->persona->apellidop}}</h3>
+                    <div class="cs-card_price">{!!$docente->perfil!!}</div>
+                    
                 </div>
             </div>
-            </div>
-        </div>
-    
+        </div><!-- .cs-slide -->
     @endforeach
+
 @stop
 
 
--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -114,9 +114,9 @@
                       </li>
                       <li><a href="contact.html">Contact</a></li>
                         -->
-                        <li><a href="{{ url('/') }}">Inicio</a></li>
-                        <li><a href="{{ route('about') }}">Acerca de Nosotros</a></li>
-                        <li><a href="{{ route('contact') }}">Contactenos</a></li>
+                        <li><a href="{{ url('/') }}" class="cs-hero_btn cs-style1 cs-color1">Inicio</a></li>
+                        <li><a href="{{ route('about') }}" class="cs-hero_btn cs-style1 cs-color1">Acerca de Nosotros</a></li>
+                        <li><a href="{{ route('contact') }}" class="cs-hero_btn cs-style1 cs-color1">Contactenos</a></li>
                     </ul>
                   </div>
                 </div>
@@ -131,72 +131,17 @@
                   </svg>  
                 </div>
                 <div class="cs-toggle_box cs-notification_box">
-                  <div class="cs-toggle_btn cs-header_icon_btn cs-center">
+                  {{-- <div class="cs-toggle_btn cs-header_icon_btn cs-center">
                     <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path d="M14 6.63916C14 5.44569 13.5259 4.30109 12.682 3.45718C11.8381 2.61327 10.6935 2.13916 9.5 2.13916C8.30653 2.13916 7.16193 2.61327 6.31802 3.45718C5.47411 4.30109 5 5.44569 5 6.63916C5 11.8892 2.75 13.3892 2.75 13.3892H16.25C16.25 13.3892 14 11.8892 14 6.63916Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
                       <path d="M10.7981 16.3887C10.6663 16.616 10.477 16.8047 10.2493 16.9358C10.0216 17.067 9.76341 17.136 9.50063 17.136C9.23784 17.136 8.97967 17.067 8.75196 16.9358C8.52424 16.8047 8.33498 16.616 8.20312 16.3887" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
                     </svg>                  
                     <span class="cs-btn_badge">8</span>
-                  </div>
-                  <div class="cs-toggle_body">
-                    <h3 class="cs-notification_title">Notifications 10</h3>
-                    <ul class="cs-notification_list">
-                      <li>
-                        <a href="#" class="cs-notification_item">
-                          <div class="cs-notification_thumb"><img src="{{asset('fronted/assets/img/general/notificaiton_1.jpeg')}}" alt="Image"></div>
-                          <div class="cs-notification_right">
-                            <p>@mark joshef purchased</p>
-                            <h4>Digital NFT Art</h4>
-                          </div>
-                        </a>
-                      </li>
-                      <li>
-                        <a href="#" class="cs-notification_item">
-                          <div class="cs-notification_thumb"><img src="{{asset('fronted/assets/img/general/notificaiton_2.jpeg')}}" alt="Image"></div>
-                          <div class="cs-notification_right">
-                            <p>@ellen capaso commented</p>
-                            <h4>Digital NFT Art</h4>
-                          </div>
-                        </a>
-                      </li>
-                      <li>
-                        <a href="#" class="cs-notification_item">
-                          <div class="cs-notification_thumb"><img src="fronted/assets/img/general/notificaiton_3.jpeg" alt="Image"></div>
-                          <div class="cs-notification_right">
-                            <p>@steve boone started following you.</p>
-                          </div>
-                        </a>
-                      </li>
-                      <li>
-                        <a href="#" class="cs-notification_item">
-                          <div class="cs-notification_thumb"><img src="fronted/assets/img/general/notificaiton_4.jpeg" alt="Image"></div>
-                          <div class="cs-notification_right">
-                            <p>@mark jos just share</p>
-                            <h4>Digital NFT Art</h4>
-                          </div>
-                        </a>
-                      </li>
-                      <li>
-                        <a href="#" class="cs-notification_item">
-                          <div class="cs-notification_thumb"><img src="fronted/assets/img/general/notificaiton_5.jpeg" alt="Image"></div>
-                          <div class="cs-notification_right">
-                            <p>@richard steger purchased</p>
-                            <h4>Digital NFT Art</h4>
-                          </div>
-                        </a>
-                      </li>
-                    </ul>
-                    <div class="text-center">
-                      <a href="#" class="cs-btn cs-style1">
-                        <span>
-                          View More
-                          <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M13.4366 7.01471C13.7295 6.72181 13.7295 6.24694 13.4366 5.95404L8.66361 1.18107C8.37072 0.888181 7.89584 0.888181 7.60295 1.18107C7.31006 1.47397 7.31006 1.94884 7.60295 2.24173L11.8456 6.48438L7.60295 10.727C7.31006 11.0199 7.31006 11.4948 7.60295 11.7877C7.89584 12.0806 8.37072 12.0806 8.66361 11.7877L13.4366 7.01471ZM0.90625 7.23438H12.9062V5.73438H0.90625V7.23438Z" fill="currentColor"/>
-                          </svg> 
-                        </span>                         
-                      </a>
-                    </div>
-                  </div>
+                  </div> --}}
+                  {{-- <div class="cs-toggle_body">
+                    <h3 class="cs-notification_title">Notificaciones 10</h3>
+                    
+                  </div> --}}
                 </div>
                 <div class="cs-toggle_box cs-profile_box">
                   <div class="cs-toggle_btn cs-header_icon_btn cs-center">
@@ -207,34 +152,14 @@
                   </div>
                   <div class="cs-toggle_body">
                     <div class="cs-user_info">
-                      <h3 class="cs-user_name">Thomas G. Smith</h3>
-                      <h4 class="cs-user_balance">13.45 ETH</h4>
+                      <h3 class="cs-user_name">Bienvenido a Instituto ITE</h3>
+                      
                       <div class="cs-user_profile_link">
-                        <div class="cs-user_profile_link_in">
-                          <span>1BAkZn7LrU43oK8Jv...</span>
-                          <button>
-                            <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-                              <path d="M4.35381 4.15531L4.35156 5.74756V13.6256C4.35156 14.272 4.60837 14.892 5.06549 15.3491C5.52261 15.8063 6.1426 16.0631 6.78906 16.0631H13.2511C13.1346 16.3921 12.9191 16.6769 12.634 16.8784C12.349 17.0799 12.0086 17.1881 11.6596 17.1881H6.78906C5.84423 17.1881 4.93809 16.8127 4.26999 16.1446C3.6019 15.4765 3.22656 14.5704 3.22656 13.6256V5.74756C3.22656 5.01256 3.69681 4.38631 4.35381 4.15531ZM13.5391 2.18506C13.7607 2.18506 13.9801 2.22871 14.1848 2.31351C14.3896 2.39832 14.5756 2.52262 14.7323 2.67932C14.889 2.83601 15.0133 3.02204 15.0981 3.22678C15.1829 3.43152 15.2266 3.65095 15.2266 3.87256V13.6226C15.2266 13.8442 15.1829 14.0636 15.0981 14.2683C15.0133 14.4731 14.889 14.6591 14.7323 14.8158C14.5756 14.9725 14.3896 15.0968 14.1848 15.1816C13.9801 15.2664 13.7607 15.3101 13.5391 15.3101H6.78906C6.34151 15.3101 5.91229 15.1323 5.59582 14.8158C5.27935 14.4993 5.10156 14.0701 5.10156 13.6226V3.87256C5.10156 3.42501 5.27935 2.99578 5.59582 2.67932C5.91229 2.36285 6.34151 2.18506 6.78906 2.18506H13.5391ZM13.5391 3.31006H6.78906C6.63988 3.31006 6.4968 3.36932 6.39132 3.47481C6.28583 3.5803 6.22656 3.72337 6.22656 3.87256V13.6226C6.22656 13.9331 6.47856 14.1851 6.78906 14.1851H13.5391C13.6882 14.1851 13.8313 14.1258 13.9368 14.0203C14.0423 13.9148 14.1016 13.7717 14.1016 13.6226V3.87256C14.1016 3.72337 14.0423 3.5803 13.9368 3.47481C13.8313 3.36932 13.6882 3.31006 13.5391 3.31006Z" fill="currentColor"/>
-                            </svg>                            
-                          </button>
-                        </div>
+                        
                       </div>
                     </div>
-                    <ul>
-                      <li><a href="user-profile.html">My Profile</a></li>
-                      <li><a href="user-items.html">My Item</a></li>
-                      <li><a href="user-wallet.html">My Wallet</a></li>
-                      <li><a href="user-account-settings.html">Settings</a></li>
-                      <li>
-                        <div class="form-check form-switch">
-                          <input class="form-check-input" type="checkbox" id="mode_switch" checked>
-                          <label class="form-check-label" for="mode_switch">Night Mode</label>
-                        </div>
-                      </li>
-                      <li><a href="login.html">Logout</a></li>
-                    </ul>
                     <div class="text-center">
-                      <a href="create-items.html" class="cs-btn cs-style1"><span>Create</span></a>
+                      <a href="create-items.html" class="cs-btn cs-style1"><span>Iniciar Sesion</span></a>
                     </div>
                   </div>
                 </div>
@@ -268,7 +193,9 @@
           <div class="cs-slider cs-style1">
             <div class="cs-slider_container" data-autoplay="0" data-loop="1" data-speed="600" data-center="1" data-slides-per-view="responsive" data-xs-slides="1" data-sm-slides="3" data-md-slides="3" data-lg-slides="3" data-add-slides="3">
               <div class="cs-slider_wrapper">
-                @yield('docente') 
+                
+                  @yield('docente')
+                
               </div>
             </div><!-- .cs-slider_container -->
             <div class="cs-slider_arrows cs-style1 cs-center cs-hidden_mobile">
