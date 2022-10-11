3b6f23968b7bddb80ce6c64820459535240ed257
institutoite <informaciones.ite@gmail.com>
2022-10-11 08:08:55 -0400

    grafica cantidad de inscripciones por modalidades

--- app/Http/Controllers/ChartController.php
+++ app/Http/Controllers/ChartController.php
@@ -7,17 +7,18 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\Inscripcione;
 
-
 class ChartController extends Controller
 {
     public function chartCantidadInscripcionesXModaalidades(){
         $consulta = Inscripcione::join('modalidads','inscripciones.modalidad_id','modalidads.id')
         ->select('modalidad',DB::raw('count(*) as cantidad'))
-        ->groupBy('modalidad')
-        ->pluck('modalidad', 'cantidad');
-        $labels = $consulta->keys();
-        $data = $consulta->values();
-        //dd($data);
-        return view('chart.inscripciones.pormodalidades',compact('labels', 'data'));
+        ->groupBy('modalidad')->get();
+        //->pluck('modalidad', 'cantidad');
+        foreach ($consulta as $elemento) {
+            $data['label'][]=$elemento->modalidad;
+            $data['data'][]=$elemento->cantidad;
+        }
+        $data['data']=json_encode($data);
+        return view('chart.inscripciones.pormodalidades',$data);
     }
 }
--- resources/views/chart/inscripciones/pormodalidades.blade.php
+++ resources/views/chart/inscripciones/pormodalidades.blade.php
@@ -26,15 +26,22 @@
             </table>
         </div>
     </div>
-    {{$labels}}
-    {{$data}}
+   
     
     <div class="card">
         <div class="card-header">
             Grafico
         </div>
         <div class="card-body">
-            <canvas id="chartmodalidades" width="400" height="400"></canvas>
+            <canvas id="chartmodalidadespie" width="400" height="400"></canvas>
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            Grafico
+        </div>
+        <div class="card-body">
+            <canvas id="chartmodalidadesbar" width="400" height="400"></canvas>
         </div>
     </div>
 @endsection
@@ -52,32 +59,27 @@
     {{-- <script src="https://www.jsdelivr.com/package/npm/chart.js?path=dist"></script> --}}
     <script>
         $(document).ready(function() {
-            const ctx = document.getElementById('chartmodalidades')
+            const ctx = document.getElementById('chartmodalidadespie')
+            const ctxbar = document.getElementById('chartmodalidadesbar')
 
-            
-
+         const cData=JSON.parse('<?php echo $data ?>');
+            console.log(cData);
             const myChart = new Chart(ctx, {
-                type: 'bar',
+                type: 'pie',
                 data: {
-                    labels: {{$labels}},
+                    labels: cData.label,
                     datasets: [{
                         label: '# of Votes',
-                        data: "{{$data}}",
+                        data: cData.data,
                         backgroundColor: [
                             'rgba(255, 99, 132, 0.2)',
                             'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
+                           
                         ],
                         borderColor: [
                             'rgba(255, 99, 132, 1)',
                             'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
+                            
                         ],
                         borderWidth: 1
                     }]
@@ -87,6 +89,39 @@
                         y: {
                             beginAtZero: true
                         }
+                    }
+                }
+            });
+            const myChartBar = new Chart(ctxbar, {
+                type: 'bar',
+                data: {
+                    labels: cData.label,
+                    datasets: [{
+                        label: 'Cantidad Inscripciones',
+                        data: cData.data,
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                           
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            
+                        ],
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            display: true,
+                            ticks: {
+                                suggestedMin: 0,    // minimum will be 0, unless there is a lower value.
+                                // OR //
+                                //beginAtZero: true   // minimum value will be 0.
+                            }
+                        }]
                     }
                 }
             });
