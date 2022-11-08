2c02a215ea25789d591adf37184d0ef800b83e9a
institutoite <informaciones.ite@gmail.com>
2022-11-08 12:26:37 -0400

    contactos corriendo slash

--- app/Http/Controllers/PersonaController.php
+++ app/Http/Controllers/PersonaController.php
@@ -1108,7 +1108,7 @@ class PersonaController extends Controller
     public function descargarContacto($persona){
         $una_una_persona=Persona::findOrFail($persona);
         // $url=storage_path("app//contactos//".$una_una_persona->nombre.$una_una_persona->id.".vcf");
-        $url=storage_path("app\contactos\\".$una_una_persona->nombre.$una_una_persona->id.".vcf");
+        $url=storage_path("app/contactos/".$una_una_persona->nombre.$una_una_persona->id.".vcf");
         dd($url);
         if(!is_null($url)){
             if (file_exists($url)){
