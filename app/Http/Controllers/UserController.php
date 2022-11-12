a8f8446653af38cf933158ba86008c57245ab7cc
institutoite <informaciones.ite@gmail.com>
2022-11-12 17:34:02 -0400

    produccion

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -98,7 +98,7 @@ class UserController extends Controller
 
         }
         $usuario->save();
-        
+        $usuario->assignRole('Docente');
         return redirect()->route('users.index')
             ->with('success', 'User created successfully.');
     }
