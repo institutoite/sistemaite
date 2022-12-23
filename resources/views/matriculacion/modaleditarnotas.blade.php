e5556599bdbd15b89d4864b9f5c20adb016b73c7
institutoite <informaciones.ite@gmail.com>
2022-12-22 21:10:16 -0400

    notas editar iniciando

--- resources/views/matriculacion/modaleditarnotas.blade.php
+++ resources/views/matriculacion/modaleditarnotas.blade.php
@@ -8,6 +8,38 @@
             <div class="modal-editar-pago">
                 <div class="container p-4">
                     <form id="form-editar-nota">
+                        <div class="row">
+                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" >
+                                <div class="form-floating mb-3 text-gray">
+                                    <input class="form-control" type="text" name="">
+                                    <label for="grado_id">asistencia</label>
+                                </div>  
+                            </div>
+                        </div> 
+                        <div class="row">
+                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" >
+                                <div class="form-floating mb-3 text-gray">
+                                    <input class="form-control" type="text" name="">
+                                    <label for="grado_id">practicos</label>
+                                </div>  
+                            </div>
+                        </div> 
+                        <div class="row">
+                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" >
+                                <div class="form-floating mb-3 text-gray">
+                                    <input class="form-control" type="text" name="">
+                                    <label for="grado_id">examen</label>
+                                </div>  
+                            </div>
+                        </div> 
+                        <div class="row">
+                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" >
+                                <div class="form-floating mb-3 text-gray">
+                                    <input class="form-control" type="text" name="">
+                                    <label for="grado_id">A tiempo</label>
+                                </div>  
+                            </div>
+                        </div> 
                         
                     </form>
                 </div>
